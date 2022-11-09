33a65931e0c308ad02f385848a2242336009ca05
Alex <30310397+sergdalm@users.noreply.github.com>
2022-11-09 16:55:45 +0300

    Added CompanyReadMapper

--- src/main/java/com/dmdev/spring/http/controller/UserController.java
+++ src/main/java/com/dmdev/spring/http/controller/UserController.java
@@ -1,6 +1,9 @@
 package com.dmdev.spring.http.controller;
 
+import com.dmdev.spring.database.entity.Role;
+import com.dmdev.spring.database.entity.User;
 import com.dmdev.spring.dto.UserCreateEditDto;
+import com.dmdev.spring.service.CompanyService;
 import com.dmdev.spring.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -15,6 +18,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/users")
@@ -22,6 +26,7 @@ import org.springframework.web.server.ResponseStatusException;
 public class UserController {
 
     private final UserService userService;
+    private final CompanyService companyService;
 
     @GetMapping
     // Методы в контроллере именуем как и раньше - с глаголами
@@ -37,10 +42,23 @@ public class UserController {
         return userService.findById(id)
                 .map(user -> {
                             model.addAttribute("user", user);
+                            model.addAttribute("roles", Role.values());
+                            model.addAttribute("companies", companyService.findAll());
                             return "user/user";
                         }
                 )
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+
+    @GetMapping("/registration")
+    // Если объекта user не будет, то все его поля будут null
+    // Тут чтобы не было проблем с неймингом мы явно используем аннотацию @ModelAttribute
+    // и указываем в скобочках имя user (потому что до этого клали этот объект в сессию с помощью метода redirectAttributes.addFlashAttribute()
+    public String registration(Model model, @ModelAttribute("user") User user) {
+        model.addAttribute("user", user);
+        model.addAttribute("roles", Role.values());
+        model.addAttribute("companies", companyService.findAll());
+        return "users/registration";
     }
 
     // Тут неявно будет использоваться @ModelAttribute
@@ -48,13 +66,25 @@ public class UserController {
     // В случае create мы должны вернуть не 200 статус, а 201,
     // для этого есть аннотация @ResponseStatus
 //    @ResponseStatus(HttpStatus.CREATED)
-    public String create(UserCreateEditDto user) {
+    public String create(UserCreateEditDto user, RedirectAttributes redirectAttributes) {
+        if (user.getUsername() != null) { // тут должна быть валидация
+            // (тут должна б
+//            return "redirect:/users/registration=" + user.getUsername();
+
+            // другой способ добавить юзера: добавить каждое поле отдельности, эти значения будут переданы в адрес строки (в параметры url)
+//            redirectAttributes.addAttribute("username", user.getUsername());
+
+            // использует не параметры в url, а обычный session attribute - этот объект будет в сессии
+            redirectAttributes.addFlashAttribute("user", user);
+
+            return "redirect:/users/registration";
+        }
         return "redirect:/users/" + userService.create(user).getId();
     }
 
     // Мы не можем из нашей формы отправлять put запрос, поэтому тут мы нарушим best practice по API и добавим "/update"
 //    @PutMapping("/{id}")
-    @PutMapping("/{id}/update")
+    @PostMapping("/{id}/update")
     public String update(@PathVariable("id") Long id, UserCreateEditDto user) {
 //        userService.update(id, user)
 
@@ -66,7 +96,7 @@ public class UserController {
     }
 
     //    @DeleteMapping("/{id}")
-    @DeleteMapping("/{id}/delete")
+    @PostMapping("/{id}/delete")
     // delete() или remove()
     public String delete(@PathVariable("id") Long id) {
 //        userService.delete(id);
